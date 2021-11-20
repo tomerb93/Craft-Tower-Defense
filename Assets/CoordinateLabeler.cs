0afc9c36eb8424779b2955ec8433fe28fdd29627
Tomer <tomerb93@gmail.com>
2021-11-20 21:28:39 +0200

    Updated grid settings
    New script CoordinateLabeler
    Added initial grid tiles and coordinate labels, 
    Update gitignore to Unity project

---
+++ Assets/CoordinateLabeler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateTileName();
+        }
+    }
+
+    void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = $"{coordinates.x},{coordinates.y}";
+    }
+    void UpdateTileName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
