5b2aeea9a5dfed5246070479202bc95b4d7412c3
Tomer <tomerb93@gmail.com>
2021-12-04 18:42:35 +0200

    Pathfinder class initial

---
+++ Assets/Pathfinding/Pathfinder.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Node currentSearchNode;
+
+
+    void Start()
+    {
+        
+    }
+
+}
